6ee929d754cd44def4ae971bdbc0256e2ed32c01
dinaaa25 <dina.lazorenko@gmail.com>
2023-12-21 20:23:15 +0100

    add joined table

--- load_genre_df.py
+++ load_genre_df.py
@@ -1,10 +1,59 @@
-from pyspark.sql import SparkSession 
+from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.getOrCreate()
+spark: SparkSession = SparkSession.builder.getOrCreate()
 
-def load_genre_df(path: str):
-    return spark.sparkContext.textFile(path).zipWithIndex().filter(lambda row: row[1] >= 7).map(lambda row: row[0]).map(lambda x: x.split("\t")).map(lambda x: (x[0], x[1])) 
+"""
+paths to data on hdfs
+"""
+genre_path = "/user/s2345846/project/msd_tagtraum_cd2.cls"
+song_path = "/data/doina/OSCD-MillionSongDataset/output_*.csv"
 
-rdd1 = load_genre_df("/user/s2345846/project/msd_tagtraum_cd2.cls")
-df = rdd1.toDF(["id", "genre"])
-df.show()
+
+class DataFrameLoader:
+    """
+    Loads the starting dataframe from the given hdfs datasets
+    """
+
+    def load_genre_df(self, path: str):
+        """
+        returns the df with all attributes from the genre dataset
+        """
+        return (
+            spark.sparkContext.textFile(path)
+            .zipWithIndex()
+            .filter(lambda row: row[1] >= 7)
+            .map(lambda row: row[0])
+            .map(lambda x: x.split("\t"))
+            .map(lambda x: (x[0], x[1]))
+            .toDF(["id", "genre"])
+        )
+
+    def load_song_df(self, path: str):
+        """
+        returns the df with all attributes from the song dataset
+        """
+        return spark.read.options(header=True, quote='"', escape='"').csv(path)
+
+    def join_dataframes(self, useAllTracks=True):
+        """
+        joins the genre df and the song df based on the track_id
+        one song has many tracks
+        useAllTracks = True keeps the songs with no matching genre lable in the joined table
+        useAllTracks = False keeps only the songs with a matching genre lable via track_id in the joined table
+        """
+        joinType = "left"
+        if not useAllTracks:
+            joinType = "inner"
+
+        genreData = self.load_genre_df(genre_path)
+        songData = self.load_song_df(song_path)
+        return songData.join(genreData, songData.track_id == genreData.id, joinType)
+
+
+"""
+preview the result of the genre df
+
+"""
+# rdd1 = load_genre_df(genre_path)
+# df = rdd1.toDF(["id", "genre"])
+# df.show()
