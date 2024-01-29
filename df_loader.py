d0c2856d9eacad99bd68f54d76ea63b1c1ef4ff6
caterina0718 <96427495+caterina0718@users.noreply.github.com>
2024-01-29 11:37:02 +0100

    df_loader used for data uploading

--- df_loader.py
+++ df_loader.py
@@ -1,25 +1,34 @@
+#-------------IMPORTS----------------------------------------------
 from pyspark.sql import SparkSession
 
+#-------------CREATE_SPARK_SESSION-----------------------------------
 # spark: spark = (
 # SparkSession.builder.appName("SongProject")
 # .master("http://spark-head5.eemcs.utwente.nl:4040")
 # .getOrCreate()
 # )
 
-spark = SparkSession.builder.remote(
-    "sc://spark-head5.eemcs.utwente.nl:4040"
-).getOrCreate()
+# spark = SparkSession.builder.remote(
+#     "sc://spark-head5.eemcs.utwente.nl:4040"
+# ).getOrCreate()
 
-"""
-paths to data on hdfs
-"""
+spark = SparkSession.builder.appName("MSD-df_load").getOrCreate()
+sc = spark.sparkContext
+
+#set log level to remove redundant logging messages
+sc.setLogLevel("ERROR")
+
+#--------------PATHS_TO_DATA--------------------------------
 genre_path = "/user/s2345846/project/msd_tagtraum_cd2.cls"
-song_path = "/data/doina/OSCD-MillionSongDataset/output_*.csv"
+song_path = "/data/doina/OSCD-MillionSongDataset/output_" #*.csv"
 
-
+#--------------DATAFRAME_LOADING---------------------------
 class DataFrameLoader:
     """
     Loads the starting dataframe from the given hdfs datasets
+    It has 2 columns:
+    id - the unique id of the song
+    genre - the genre taken from the genre_path
     """
 
     def load_genre_df(self, path: str):
@@ -38,7 +47,7 @@ class DataFrameLoader:
 
     def load_genre_types_df(self):
         """
-        returns a list with the unique genretypes
+        returns a list with the unique genre types
         """
         genreData = self.load_genre_df(genre_path)
         distinctDF = genreData.select("genre").distinct()
@@ -50,7 +59,7 @@ class DataFrameLoader:
         """
         return spark.read.options(header=True, quote='"', escape='"').csv(path)
 
-    def join_dataframes(self, useAllTracks=True):
+    def join_dataframes(self, partial_song_path, useAllTracks=True):
         """
         joins the genre df and the song df based on the track_id
         one song has many tracks
@@ -62,17 +71,43 @@ class DataFrameLoader:
             joinType = "inner"
 
         genreData = self.load_genre_df(genre_path)
-        songData = self.load_song_df(song_path)
+        songData = self.load_song_df(partial_song_path)
+
+        # optionally select only a few columns to keep
+        #songData = songData.select("track_id")
         return songData.join(genreData, songData.track_id == genreData.id, joinType)
-
-
-"""
-preview the result of the genre df
-
-"""
 
 dataframeloader = DataFrameLoader()
 
-rdd1 = dataframeloader.load_genre_df(genre_path)
-df = rdd1.toDF("id", "genre")
-df.show()
+#------------------PREVIEW_DATA-------------------------------------------------
+"""
+Preview the result of the genre df
+"""
+#df_genre = dataframeloader.load_genre_df(genre_path)
+#df_genre.sort("id").show(10)
+
+"""
+Preview the result of the joined df
+"""
+# df_joined = dataframeloader.join_dataframes(useAllTracks=False)
+# df_joined.count()
+# #df_joined.show(10)
+
+"""
+Preview the result of the song df
+"""
+#df_song = dataframeloader.load_song_df(song_path).select("artist_terms").sort("artist_terms")
+#df_song.show(10)
+
+#------------------SAVE_DATAFRAME--------------------------------------------
+"""
+Export a dataframe containing the song id and corresponding genres to parquet files 
+"""
+for p in range(5):
+    song_path_full = song_path + (str(chr(ord('A') + p)) + ".csv")
+    df_for_ml = dataframeloader.join_dataframes(song_path_full, useAllTracks=False)
+    print(df_for_ml.count()) #-> 38367, 36618, 35955, 35724, 35241 tracks in the sets
+
+    #df_for_ml = df_joined
+    save_folder = "/user/s2549182/MBD/id_genres_" + str(chr(ord('A') + p))
+    df_for_ml.write.parquet(path=save_folder, mode="overwrite")
